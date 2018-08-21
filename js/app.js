f2e1df890bcef5aed93bb34cca10eec541f592c3
Sean Tayler <seanjtayler@gmail.com>
2018-08-21 13:59:13 -0700

    finished project

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 (function() {
   'use strict';
 
-  const movies = [];
+  let movies;
 
-  const renderMovies = function() {
+  const renderMovies = function(arr) {
     $('#listings').empty();
 
-    for (const movie of movies) {
+    for (const movie of arr) {
       const $col = $('<div>').addClass('col s6');
       const $card = $('<div>').addClass('card hoverable');
       const $content = $('<div>').addClass('card-content center');
@@ -14,16 +14,16 @@
 
       $title.attr({
         'data-position': 'top',
-        'data-tooltip': movie.title
+        'data-tooltip': movie.Title
       });
 
-      $title.tooltip({ delay: 50 }).text(movie.title);
+      $title.tooltip({ delay: 50 }).text(movie.Title);
 
       const $poster = $('<img>').addClass('poster');
 
       $poster.attr({
-        src: movie.poster,
-        alt: `${movie.poster} Poster`
+        src: movie.Poster,
+        alt: `${movie.Poster} Poster`
       });
 
       $content.append($title, $poster);
@@ -41,9 +41,9 @@
 
       const $modal = $('<div>').addClass('modal').attr('id', movie.id);
       const $modalContent = $('<div>').addClass('modal-content');
-      const $modalHeader = $('<h4>').text(movie.title);
-      const $movieYear = $('<h6>').text(`Released in ${movie.year}`);
-      const $modalText = $('<p>').text(movie.plot);
+      const $modalHeader = $('<h4>').text(movie.Title);
+      const $movieYear = $('<h6>').text(`Released in ${movie.Year}`);
+      const $modalText = $('<p>').text(movie.Plot);
 
       $modalContent.append($modalHeader, $movieYear, $modalText);
       $modal.append($modalContent);
@@ -56,5 +56,26 @@
     }
   };
 
-  // ADD YOUR CODE HERE
+  let search = document.getElementById('search');
+  let submit = document.getElementById('submit');
+  let listings = document.getElementById('listings');
+
+  let getResults = function(e) {
+    e.preventDefault();
+    if (search.value !== '') {
+      list('DOmCL')
+      fetch(`http://www.omdbapi.com/?s=${search.value}&apikey=8e7e3c3e`)
+      // fetch(`http://img.omdbapi.com/?apikey=8e7e3c3e&`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.Search)
+        movies = data.Search
+        renderMovies(movies)
+      });
+    }
+    search.value = '';
+  }
+
+  submit.addEventListener('click', getResults);
+
 })();
